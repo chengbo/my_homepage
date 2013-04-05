bd62a9fec837e116f0d0dfbfcf6857d42749b0c8
bo cheng <chengbo1983@gmail.com>
2013-04-05 14:05:47 -0700

    fix a route bug causing static files are not found

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
   app.use(express.cookieParser('your secret here'));
   app.use(express.session());
-app.use(app.router);
   app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(app.router);
 
 // development only
 if ('development' == app.get('env')) {
